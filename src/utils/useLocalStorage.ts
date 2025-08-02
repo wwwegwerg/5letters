ece2f43f5731951429dcd95825a8d233e307c97a
Daniil <xmegatech89@gmail.com>
2025-08-02 19:02:26 +0500

    added callback

--- src/utils/useLocalStorage.ts
+++ src/utils/useLocalStorage.ts
@@ -1,7 +1,10 @@
-import { useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 
-export function useLocalStorage(key: string, initialValue: unknown) {
-  const [storedValue, setStoredValue] = useState(() => {
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T,
+): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const [storedValue, setStoredValueRaw] = useState<T>(() => {
     try {
       const item = localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
@@ -10,12 +13,22 @@ export function useLocalStorage(key: string, initialValue: unknown) {
       return initialValue;
     }
   });
-  useEffect(() => {
-    try {
-      localStorage.setItem(key, JSON.stringify(storedValue));
-    } catch (e) {
-      console.warn(e);
-    }
-  }, [key, storedValue]);
+  const setStoredValue = useCallback<React.Dispatch<React.SetStateAction<T>>>(
+    (value) => {
+      setStoredValueRaw((prev) => {
+        const valueToStore =
+          typeof value === "function"
+            ? (value as (prevState: T) => T)(prev)
+            : value;
+        try {
+          localStorage.setItem(key, JSON.stringify(valueToStore));
+        } catch (e) {
+          console.warn(e);
+        }
+        return valueToStore;
+      });
+    },
+    [key],
+  );
   return [storedValue, setStoredValue];
 }
