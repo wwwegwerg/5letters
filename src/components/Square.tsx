aeaa1aabd9f0d2fe19bf8926353d1faa362e631b
Daniil <xmegatech89@gmail.com>
2025-08-02 19:02:40 +0500

    changed styles

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -8,15 +8,22 @@ export function Square({
   status: LetterStatus;
 }) {
   const bgColor = status
-    ? status == "absent"
+    ? status === "absent"
       ? "bg-gray-500"
-      : status == "present"
+      : status === "present"
         ? "bg-yellow-500"
         : "bg-green-500"
     : "bg-white";
+  const borderColor = status
+    ? status === "absent"
+      ? "border-gray-500"
+      : status === "present"
+        ? "border-yellow-500"
+        : "border-green-500"
+    : "border-[#1C1C1E]";
   return (
     <span
-      className={`block h-16 w-16 rounded-sm border-4 border-[#1C1C1E] text-center text-5xl leading-14 font-bold lg:h-18 lg:w-18 lg:text-6xl lg:leading-16 ${bgColor}`}
+      className={`block h-16 w-16 rounded-sm border-4 text-center text-5xl leading-14 font-bold lg:h-18 lg:w-18 lg:text-6xl lg:leading-16 ${bgColor} ${borderColor}`}
     >
       {value}
     </span>
