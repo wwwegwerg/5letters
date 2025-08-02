ca3c9b1df11536028e4eda346e193c498a39c552
Daniil <xmegatech89@gmail.com>
2025-08-02 20:31:13 +0500

    updated type

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,17 @@
+import type React from "react";
+
 export function Button({
   onClick,
   disabled = false,
   value,
-}: {
-  onClick: () => void;
-  disabled?: boolean;
-  value: string;
-}) {
+  type = "button",
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
       className="rounded-sm border-2 border-[#1C1C1E] px-3 font-bold"
+      type={type}
     >
       {value}
     </button>
